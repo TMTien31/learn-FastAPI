f76441d147cd54a7ff1a9f28fbbf297260e75b83
Tran Minh Tien <tmt31032005@gmail.com>
2025-08-16 17:25:02 +0700

    create table User

--- blog/main.py
+++ blog/main.py
@@ -1,3 +1,4 @@
+from typing import List
 from fastapi import FastAPI, Depends, status, Response, HTTPException
 from . import schemas, models
 from .database import engine, SessionLocal
@@ -35,7 +36,7 @@ def destroy(id: int, db: Session = Depends(get_db)):
 
 @app.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED)
 def update(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
-    
+
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
         raise HTTPException(status_code=404, detail=f"Blog {id} not found")
@@ -45,12 +46,12 @@ def update(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
     return 'updated successfully'
     
 
-@app.get("/blog")
+@app.get("/blog", response_model=List[schemas.ShowBlog])
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get("/blog/{id}", status_code=200)
+@app.get("/blog/{id}", status_code=200, response_model=schemas.ShowBlog)
 def show(id: int, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
@@ -58,3 +59,11 @@ def show(id: int, response: Response, db: Session = Depends(get_db)):
         #response.status_code = status.HTTP_404_NOT_FOUND
         #return {"detail": f"Blog {id} not found"}
     return blog
+
+@app.post('/user')
+def create_user(request: schemas.User, db: Session = Depends(get_db)):
+    new_user = models.User(name=request.name, email=request.email, password=request.password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
